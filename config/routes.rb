bb4f21ac2848e3b280c5affc8abfec8ea5c32dee
PJ Mantoss <joelptoss@gmail>
2019-07-05 20:08:14 +0100

    Add secure password and resource for students and courses

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  root 'courses#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'pages#about'
+  get 'courses/new', to: 'courses#new'
+  resources :students, except: [:destroy]
 end
